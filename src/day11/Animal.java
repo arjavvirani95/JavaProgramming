416da7a79601af8bd7adcb86b656ceb3316de11b
arjavvirani95 <arjav.virani17@gmail.com>
2024-01-09 14:43:31 -0500

    OOP concepts

---
+++ src/day11/Animal.java
@@ -0,0 +1,31 @@
+package day11;
+
+public class Animal {
+
+	String color="white";
+	
+	void eat()
+	{
+		System.out.println("eating.....");
+	}
+	
+}
+
+
+
+class Dog extends Animal
+{
+	String color="black";
+	
+	
+	void displayColor()
+	{
+		System.out.println(super.color);
+	}
+	
+	void eat()
+	{
+		System.out.println("eating bread");
+		super.eat();
+	}
+}
---
+++ src/day15/ArrayListDemo.java
@@ -0,0 +1,66 @@
+package day15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*1) Heterogeneous data - allowed
+2) Insertion order- preserved(Index)
+3) Duplicate elements -- allowed
+4) multiple nulls -- allowed */
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+	
+		//declaration
+		ArrayList mylist1=new ArrayList(); //It will store Heterogeneous data
+		//ArrayList <String>mylist=new ArrayList<String>();  // only strings
+		//List mylist1=new ArrayList(); //mylist1 is object of ArrayList class refer to List interface
+
+		
+		//adding data elements into arraylist
+		mylist1.add(100);
+		mylist1.add(10.5);
+		mylist1.add("welcome");
+		mylist1.add(true);
+		mylist1.add(100);
+		mylist1.add(null);
+		mylist1.add(null);
+
+		//size of arraylist
+		int myListSize = mylist1.size();
+		System.out.println(mylist1.size());//7
+		
+		System.out.println(mylist1);  //[100, 10.5, welcome, true, 100, null, null]
+		
+		//remove value from arraylist
+		mylist1.remove(3);
+		System.out.println("After removing element:"+mylist1);  //[100, 10.5, welcome, 100, null, null]
+		
+		//inserting new element in the middle of the list
+		mylist1.add(2,"java");
+		
+		System.out.println("after insertion:"+mylist1);  //[100, 10.5, java, welcome, 100, null, null]
+		
+		System.out.println(mylist1.get(3));
+		
+		//read all the data from arraylist
+		
+		for(int j=0;j<myListSize;j++) {
+			try {
+				System.out.println(mylist1.get(j));
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		/*for(Object x:mylist1)
+		{
+			System.out.println(x);
+		}*/
+		
+		mylist1.clear(); //clears the data from arraylist
+		System.out.println("After clearing "+ mylist1);  // []
+	}
+
+}
