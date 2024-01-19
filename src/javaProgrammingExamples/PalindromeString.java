60718e6b7202b403e0d17fd60fb7286e86ff554a
arjavvirani95 <arjav.virani17@gmail.com>
2024-01-19 09:23:35 -0500

    updated palindrome file

--- src/javaProgrammingExamples/PalindromeString.java
+++ src/javaProgrammingExamples/PalindromeString.java
@@ -3,36 +3,61 @@ package javaProgrammingExamples;
 public class PalindromeString {
 	
 	public static void main(String[] args) {
-			
-				String s = "DAD";
-				
-				//1. using for loop 
-				int len = s.length(); //7
-				String rev = "";
-				
-				for(int i =len-1; i>=0; i--){
-					rev = rev + s.charAt(i); //muineleS
-				}
-				
-				System.out.println(rev);
-				
-				if(s.equals(rev))
-				{
-					System.out.println("Palindrome string");
-				}
-				else
-				{
-					System.out.println("Not Palindrome string");
-				}
-				
-				
-				
-				
-				
-				//2. using StringBuffer class:
-				//StringBuffer sf = new StringBuffer(s);
-				//System.out.println(sf.reverse());
+
+		//For single word string 
+		String s = "DAD";
+
+		//1. using for loop 
+		int len = s.length(); //7
+		String rev = "";
+
+		for(int i =len-1; i>=0; i--){
+			rev = rev + s.charAt(i); //muineleS
+		}
+
+		System.out.println(rev);
+
+		if(s.equals(rev))
+		{
+			System.out.println("Palindrome string");
+		}
+		else
+		{
+			System.out.println("Not Palindrome string");
+		}
+
+		//for long string splt word and find palindrome
+		String inputString = "abba is mam";
+		String[] words = inputString.split(" ");
+	 
+		for (String word : words) {
+
+			if (isPalindrome(word)) {
+				System.out.println(word);
+			}
+
+		}
+		
+		//2. using StringBuffer class:
+			//StringBuffer sf = new StringBuffer(s);
+		//System.out.println(sf.reverse());
 	}
-	
-	
+
+	private static boolean isPalindrome(String word) {
+		int len = word.length();
+		String rev = "";
+		for(int i =len-1; i>=0; i--){
+			rev = rev + word.charAt(i); //muineleS
+		}
+		if(word.equals(rev))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		} 
+	}
+
+
 }
