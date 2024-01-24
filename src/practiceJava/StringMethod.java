30148b4c73021e82e8ff3d4ec1c26a4178ebe612
arjavvirani95 <arjav.virani17@gmail.com>
2024-01-24 11:23:21 -0500

    small changes

---
+++ src/practiceJava/StringMethod.java
@@ -0,0 +1,14 @@
+package practiceJava;
+
+public class StringMethod {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Java";
+	    String str2 = "Java";
+	    String str3 = new String("Java");
+
+	    System.out.println(str1.equals(str2) && str1.equals(str3));
+	}
+
+}
