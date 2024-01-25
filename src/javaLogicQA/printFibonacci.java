72c47e50c3295280bbd8651ce63c61f1094f1969
arjavvirani95 <arjav.virani95@gmail.com>
2024-01-25 13:04:54 -0500

    small changes fibbo

--- src/javaLogicQA/printFibonacci.java
+++ src/javaLogicQA/printFibonacci.java
@@ -5,13 +5,13 @@ public class printFibonacci {
 	public static void main(String[] args) {
 		// Print Fibonacci series i.e 0,1,1,2,3,5,8,13,21
 		
-		int n = 10;
-		int series = 0;
-		int i = 0;
-		int j = 1;
-		int k = 0;
+		int n = 10; //upto what number
+		int series = 0; //third variable
+		int i = 0; //first number
+		int j = 1; //second number
+		int k = 0; //counter
 		while(k<10) {
-			System.out.print(i+" ");
+			System.out.print(i+" "); //print number
 			series = i+j;
 			i=j;
 			j=series;
