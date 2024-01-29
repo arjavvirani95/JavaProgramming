b75ee73b5c030afa84e8e675bdcd9e22021ff3fa
Arjavkumar virani <arjav.virani17@gmail.com>
2024-01-29 11:16:27 -0500

    --date=1 day ago
    2024

--- src/javaLogicQA/countOccuranceString.java
+++ src/javaLogicQA/countOccuranceString.java
@@ -5,7 +5,7 @@ public class countOccuranceString {
 	public static void main(String[] args) {
 		
 		
-		int count = countStringOccurances("arjav",'a');
+		int count = countStringOccurances("arjav",'j');
 	}
 
 	private static int countStringOccurances(String name, char fchar) {
