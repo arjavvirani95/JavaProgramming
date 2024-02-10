d38e1d6b8fe69bd51e1e953756243329a9b8d5c0
Arjavkumar virani <arjav.virani17@gmail.com>
2024-02-10 10:52:16 -0500

    update count occurance

--- src/javaLogicQA/EvenAndOddNumbersinArray.java
+++ src/javaLogicQA/EvenAndOddNumbersinArray.java
@@ -4,7 +4,7 @@ public class EvenAndOddNumbersinArray {
 
 	public static void main(String[] args) {
 		
-		int a[]={10,20,15,3,6,7,8,2,5,7,10,58,95,96,16};
+		int a[]={10,20,15,3,6,7,8,2,5,7,10,58,95,96,16,57,98,21,20,64,58,97,93,49};
 		
 		int n = a.length;
 		System.out.print("Odd number: ");
