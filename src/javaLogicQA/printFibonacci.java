bb3cdc94ba9016039daea0a4ab8cdbffcb632950
Arjavkumar virani <arjav.virani17@gmail.com>
2024-02-12 10:55:19 -0500

    update fibonnaci series

--- src/javaLogicQA/printFibonacci.java
+++ src/javaLogicQA/printFibonacci.java
@@ -5,12 +5,12 @@ public class printFibonacci {
 	public static void main(String[] args) {
 		// Print Fibonacci series i.e 0,1,1,2,3,5,8,13,21
 		
-		int n = 10; //upto what number
+		int n = 15; //upto what number
 		int series = 0; //third variable
 		int i = 0; //first number
 		int j = 1; //second number
 		int k = 0; //counter
-		while(k<10) {
+		while(k<n) {
 			System.out.print(i+" "); //print number
 			series = i+j;
 			i=j;
