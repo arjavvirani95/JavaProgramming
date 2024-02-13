9e89d79d36051d0743ff6b38a7bf15394aede26e
Arjavkumar virani <arjav.virani17@gmail.com>
2024-02-13 10:56:33 -0500

    update sum of element in array

--- src/javaLogicQA/sumOfElements.java
+++ src/javaLogicQA/sumOfElements.java
@@ -3,7 +3,7 @@ package javaLogicQA;
 public class sumOfElements {
 
 	public static void main(String[] args) {
-		int [] a = {1,2,3,4};
+		int [] a = {1,2,3,4,9,10,12,56};
 
 		int sum = sumArray(a);
 		System.out.println(sum);
