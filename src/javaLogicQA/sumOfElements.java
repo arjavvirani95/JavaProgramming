e4887b2185b2efc0e842fa32e0602389fadd17f1
Arjavkumar virani <arjav.virani17@gmail.com>
2024-02-19 12:58:56 -0500

    change in java logic

--- src/javaLogicQA/sumOfElements.java
+++ src/javaLogicQA/sumOfElements.java
@@ -3,7 +3,7 @@ package javaLogicQA;
 public class sumOfElements {
 
 	public static void main(String[] args) {
-		int [] a = {1,2,3,4,9,10,12,56};
+		int [] a = {1,2,3,4,9,10,12,56,548,98,63,78};
 
 		int sum = sumArray(a);
 		System.out.println(sum);
