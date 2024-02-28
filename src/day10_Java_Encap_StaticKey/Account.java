4367a5d45e884056e16ce3ddec3084a0b080316d
Arjavkumar virani <arjav.virani17@gmail.com>
2024-02-28 17:15:15 -0500

    revised java encapsulation and static keyword

--- src/day10_Java_Encap_StaticKey/Account.java
+++ src/day10_Java_Encap_StaticKey/Account.java
@@ -3,34 +3,34 @@ package day10_Java_Encap_StaticKey;
 //wrapping up of data and methods in to single unit(class). 
 //all variables should be private. 
 //we can access variables only through methods(setters & getters)
- 
+
 public class Account {
 
 	private int acc_no;
 	private String name;
 	private double amount;
-	
+
 	public int getAcc_no() {
 		return acc_no;
 	}
 	public void setAcc_no(int acc_no) { //setting acc no which is coming from main class!
 		this.acc_no = acc_no; //this keyword refers instance variable not local variable {}
 	}
-	
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getAmount() {
 		return amount;
 	}
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
-	
-		
-	
+
+
+
 }
--- src/day10_Java_Encap_StaticKey/AccountMain.java
+++ src/day10_Java_Encap_StaticKey/AccountMain.java
@@ -4,18 +4,18 @@ public class AccountMain {
 
 	public static void main(String[] args) 
 	{
-		
-	Account	accobj=new Account();
-	
-	accobj.setAcc_no(1234); //sending acc value to Account class with Account class object accobj.
-	accobj.setName("John");
-	accobj.setAmount(50000);
-	
-	System.out.println(accobj.getAcc_no()); //getting value of accno through object of class Account
-	System.out.println(accobj.getName());
-	System.out.println(accobj.getAmount());
-	
-	
+
+		Account	accobj=new Account();
+
+		accobj.setAcc_no(1234); //sending acc value to Account class with Account class object accobj.
+		accobj.setName("John");
+		accobj.setAmount(50000);
+
+		System.out.println(accobj.getAcc_no()); //getting value of accno through object of class Account
+		System.out.println(accobj.getName());
+		System.out.println(accobj.getAmount());
+
+
 	}
 
 }
