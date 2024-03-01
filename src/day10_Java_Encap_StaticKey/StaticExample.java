8a8ff397259086f31a55c67d9e7a61a431ec72f9
Arjavkumar virani <arjav.virani17@gmail.com>
2024-03-01 17:17:34 -0500

    revised java static keyword

--- src/day10_Java_Encap_StaticKey/StaticExample.java
+++ src/day10_Java_Encap_StaticKey/StaticExample.java
@@ -13,27 +13,27 @@ public class StaticExample {
 
 	static int a=10;   // static variable
 	int b=20;  // non-static variable
-	
-	
+
+
 	static void m1()   // static method
 	{
 		System.out.println("this is m1 static method");
 	}
-	
+
 	void m2()   // non-static method
 	{
 		System.out.println("this is m2 non-static method");
 	}
-	
-	
+
+
 	void m()   // non-static method
 	{
 		System.out.println(a);
 		System.out.println(b);
-		
+
 		m1();
 		m2();
-		
+
 	}  
 
 }
--- src/day10_Java_Encap_StaticKey/StaticMain.java
+++ src/day10_Java_Encap_StaticKey/StaticMain.java
@@ -8,19 +8,19 @@ public class StaticMain {
 
 		System.out.println(StaticExample.a);
 		//System.out.println(b); // incorrect, bcoz b is non-static varaible
-		
+
 		StaticExample.m1();
 		//m2();   // incorrect , bcoz m2() is non-static method
-		
-		
-		
+
+
+
 		//2) static methods can access non-static stuff through object
-		
+
 		StaticExample  se=new StaticExample();
 		System.out.println(se.b);
 		se.m2();
-		
+
 		se.m();
 	}
-	
+
 }
