60d3e1efcd5aa08e0ef90047479d7df047166663
Arjavkumar virani <arjav.virani17@gmail.com>
2024-03-07 17:46:11 -0500

    revised java object calling part2

--- src/day6_Java_ObjectCalling/Student.java
+++ src/day6_Java_ObjectCalling/Student.java
@@ -4,29 +4,26 @@ public class Student {
 
 	int sid;
 	String sname;
-	char grade;
-	
+	String grade;
+
 	void show()
 	{
 		System.out.println(sid+"   "+sname+"   "+grade);
 	}
-	
-	void setData(int id, String name, char g)
-	{
-		sid=id;
-		sname=name;
-		grade=g;
-		
-	}
-	
 
-	Student(int id, String name, char g)
+	void setData(int id, String name, String g)
 	{
 		sid=id;
 		sname=name;
 		grade=g;
-		
+
 	}
-	
-	
+
+	Student(int id, String name, String g)
+	{
+		sid=id;
+		sname=name;
+		grade=g;
+
+	}
 }
--- src/day6_Java_ObjectCalling/StudentMain.java
+++ src/day6_Java_ObjectCalling/StudentMain.java
@@ -4,25 +4,22 @@ public class StudentMain {
 
 	public static void main(String[] args) 
 	{
-		
+
 		//Student stu=new Student();
-		
+
 		//Apprach1: using reference variable
 		/*stu.sid=102;
 		stu.sname="John";
 		stu.grade='A';
-		*/
-		
+		 */
+
 		//Appraoch 2: using method
 		//stu.setData(102,"John",'A');
-		
-		//Appraoch 3 : using constructor	
-		Student stu=new Student(102,"John",'A');
-		
-		
-		stu.show();
-		
-		
-	}
 
+		//Appraoch 3 : using constructor	
+		Student stu=new Student(101,"Riddhi","A+");
+
+		stu.show();
+
+	}
 }
