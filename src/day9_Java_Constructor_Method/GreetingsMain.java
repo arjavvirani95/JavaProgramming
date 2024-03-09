b5c528cf6f8718dd6e5818cc6c81bb23f56ecfe1
Arjavkumar virani <arjav.virani17@gmail.com>
2024-03-09 17:54:20 -0500

    revised java constructor method

--- src/day9_Java_Constructor_Method/GreetingsMain.java
+++ src/day9_Java_Constructor_Method/GreetingsMain.java
@@ -6,17 +6,18 @@ public class GreetingsMain {
 	{
 		Greetings gr=new Greetings();
 		//gr.greeting1();    //1
-		
+
 		//String str=gr.greeting2(); //2
 		//System.out.println(str);
-		
+
 		//System.out.println(gr.greeting2());  //2
-		
+
 		//gr.greeting3("John");  //3
-		
-		String str=gr.greeting4("John");
+
+		System.out.println(gr.greeting2());
+		String str=gr.greeting4("Arjav");
 		System.out.println(str);
-		
+
 	}
 
 }
