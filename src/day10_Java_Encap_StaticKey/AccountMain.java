c8abd4b8516d439c8dc0c21d1bc1c16009139bf2
Arjavkumar virani <arjav.virani17@gmail.com>
2024-03-17 14:30:35 -0400

    Java Revision Encap

--- src/day10_Java_Encap_StaticKey/AccountMain.java
+++ src/day10_Java_Encap_StaticKey/AccountMain.java
@@ -7,9 +7,9 @@ public class AccountMain {
 
 		Account	accobj=new Account();
 
-		accobj.setAcc_no(1234); //sending acc value to Account class with Account class object accobj.
-		accobj.setName("John");
-		accobj.setAmount(50000);
+		accobj.setAcc_no(5267893); //sending acc value to Account class with Account class object accobj.
+		accobj.setName("Arjav");
+		accobj.setAmount(100000);
 
 		System.out.println(accobj.getAcc_no()); //getting value of accno through object of class Account
 		System.out.println(accobj.getName());
