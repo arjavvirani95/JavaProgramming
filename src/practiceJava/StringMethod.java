995c30eaad164b730cf4af38866c7f35c2196db6
Arjavkumar virani <arjav.virani17@gmail.com>
2024-06-03 10:46:21 -0400

    java practice

--- src/practiceJava/StringMethod.java
+++ src/practiceJava/StringMethod.java
@@ -6,7 +6,7 @@ public class StringMethod {
 		
 		String str1 = "python";
 	    String str2 = "python";
-	    String str3 = new String("Java");
+	    String str3 = new String("python");
 
 	    System.out.println(str1.equals(str2) && str1.equals(str3));
 	}
